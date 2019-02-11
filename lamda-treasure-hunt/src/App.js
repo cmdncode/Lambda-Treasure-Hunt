51dba7e241edd579fc7da6166756d3b5a887a112
Barbara Hunter <commandandcode@gmail.com>
2019-02-11 15:26:12 -0500

    update

--- lamda-treasure-hunt/src/App.js
+++ lamda-treasure-hunt/src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import './index.css';
+import Button from 'react-bootstrap/Button';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header>Hello!!!</header>
+        <header>
+        <div className="container">
+          <h1>Lambda Treasure Hunt</h1>
+          <nav>
+            <Button href="#">Map </Button>
+            <Button href="#">About </Button>
+          </nav>
+        </div>
+        </header>
       </div>
     );
   }}
