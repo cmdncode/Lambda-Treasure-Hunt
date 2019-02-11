d4df633010dfa2111fe9f0dccfb2cc9ecda39adb
Barbara Hunter <commandandcode@gmail.com>
2019-02-11 16:02:06 -0500

    attempting to insert page tabs

--- lamda-treasure-hunt/src/App.js
+++ lamda-treasure-hunt/src/App.js
@@ -1,22 +1,38 @@
 import React, { Component } from 'react';
 import './index.css';
 import Button from 'react-bootstrap/Button';
+import Tabs from 'react-bootstrap/Tabs'
+import Tab from 'react-bootstrap/Tab'
 
+class ControlledTabs extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      key: 'home',
+    };
+  }
+}
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header>
-        <div className="container">
-          <h1>Lambda Treasure Hunt</h1>
-          <nav>
-            <Button href="#">Map </Button>
-            <Button href="#">About </Button>
-          </nav>
-        </div>
-        </header>
-      </div>
-    );
-  }}
+    render() {
+      return (
+        <Tabs
+          id="controlled-tab"
+          activeKey={this.state.key}
+          onSelect={key => this.setState({ key })}
+        >
+          <Tab eventKey="home" title="Home">
+            <h2>1</h2>
+          </Tab>
+          <Tab eventKey="profile" title="Profile">
+          <h2>2</h2>
+          </Tab>
+          <Tab eventKey="contact" title="Contact" disabled>
+          <h2>3</h2>
+          </Tab>
+        </Tabs>
+      );
+    }
+  }
 export default App;
+
