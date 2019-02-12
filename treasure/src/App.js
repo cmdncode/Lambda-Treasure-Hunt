8443d98ed7cdccaefebb02418e61ea39d8e8b30b
Barbara Hunter <commandandcode@gmail.com>
2019-02-12 13:44:01 -0500

    added title component and rendered title

--- treasure/src/App.js
+++ treasure/src/App.js
@@ -1,16 +1,11 @@
 import React from "react";
+import Titles from "./components/titles"
 
 class App extends React.Component {
   render() {
     return(
       <div>
-        Hello
-
-
-
-
-
-        
+        <Titles />
       </div>
     );
   }
---
+++ treasure/src/components/titles.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+class Titles extends React.Component {
+  render (){
+    return(
+      <div>
+        <h1>Lambda Treasure Map</h1>
+        <p>Explore the map and collect the hidden treasure!</p>
+      </div>
+      
+
+    );
+  }
+}
+
+export default Titles;
