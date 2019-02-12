779feec19d0f26ebeba441c530d2800746391c40
Barbara Hunter <commandandcode@gmail.com>
2019-02-12 13:50:51 -0500

    added form and map components, rendered both

--- treasure/src/App.js
+++ treasure/src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
-import Titles from "./components/titles"
+import Titles from "./components/titles";
+import Form from "./components/form";
+import Map from "./components/map";
 
 class App extends React.Component {
   render() {
     return(
       <div>
         <Titles />
+        <Form />
+        <Map />
       </div>
     );
   }
