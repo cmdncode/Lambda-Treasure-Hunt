dbadf5a111587acde8023b4e4641a110cf311879
Barbara Hunter <commandandcode@gmail.com>
2019-02-12 18:39:07 -0500

    axios working

--- treasure/src/components/traversal.js
+++ treasure/src/components/traversal.js
@@ -5,26 +5,40 @@ import axios from 'axios'
 export default class Traverse extends Component {
   constructor (props) {
     super(props)
+    this.state={
+      room_id: 0
+    }
   }
   componentDidMount () {
+    const header = {
+      headers: {Authorization: "Token 5f617e0516906e55ab4530499df985499805a4db"}
+    }
     axios
-      .get('https://lambda-treasure-hunt.herokuapp.com/api/adv/init/')
+      .get('https://lambda-treasure-hunt.herokuapp.com/api/adv/init/', header)
       .then((response) => {
-        console.log('connecting to server')
+        console.log(response.data)
+        this.setState({
+          room_id: response.data.room_id
+        })
       })
       .catch((error) => {
         console.log('error, could not connect to server')
       })
   }
   handleMovement (direction) {
+    const header = {
+      headers: {Authorization: "Token 5f617e0516906e55ab4530499df985499805a4db"}
+    }
     axios
       .post('https://lambda-treasure-hunt.herokuapp.com/api/adv/move/', {
         direction: direction
-      })
+      },header)
       .then((res) => console.log('posting to server...'))
       .catch((err) => console.error('could not post to server...'))
   }
   render () {
-    return (<p>Traversal Placeholder</p>)
+    return (<div>
+      room_id:{this.state.room_id}
+    </div>)
   }
 }
