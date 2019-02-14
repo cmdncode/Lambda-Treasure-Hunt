86564d049098669b5680846e7409cdcd4f4bd842
Barbara Hunter <commandandcode@gmail.com>
2019-02-14 18:39:02 -0500

    no errors

--- treasure/src/App.js
+++ treasure/src/App.js
@@ -3,6 +3,7 @@ import Titles from './components/titles'
 import Stats from './components/stats'
 import Map from './components/map'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import ReactCytoscape from 'react-cytoscape';
 
 
 
@@ -13,6 +14,10 @@ export default class App extends Component {
         <Titles />
         <Stats />
         <Map />
+        <ReactCytoscape containerID="cy" 
+        elements={this.getElements()} 
+        cyRef={(cy) => { this.cy = cy; console.log(this.cy) }} 
+        layout={{name: 'dagre'}} />  
       </div>
     );
   };
