f7783a106f91dff0f47b1da56adf7d24305647ec
Barbara Hunter <commandandcode@gmail.com>
2019-02-15 14:52:42 -0500

    sorry- but its not happening

--- treasure/src/App.js
+++ treasure/src/App.js
@@ -3,7 +3,6 @@ import Titles from './components/titles'
 import Stats from './components/stats'
 import Map from './components/map'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import ReactCytoscape from 'react-cytoscape';
 
 
 
@@ -14,10 +13,7 @@ export default class App extends Component {
         <Titles />
         <Stats />
         <Map />
-        {/* <ReactCytoscape containerID="map" 
-        elements={this.getElements} 
-        cyRef={(map) => { this.map = map; console.log(this.map) }} 
-        layout={{name: 'ROOM'}} />   */}
+        
       </div>
     );
   };
